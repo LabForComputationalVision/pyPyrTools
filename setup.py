acf527443f55d7cda8b93394ab55a5a0758e849d
nlw0 <nwerneck@gmail.com>
2017-10-09 22:00:01 +0200

    trying to make a more structured library, still not sure how to deal with the c shared library

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup, Extension
+
+from setuptools import (
+    find_packages,
+)
+
+setup(
+    name="pyPyrTools",
+    version="1.0",
+    url="https://github.com/LabForComputationalVision/pyPyrTools",
+    description="Steerable pyramid library for image analysis",
+    long_description="""pyPyrTools implements the steerable pyramid filter bank created by Simoncelli et alii in the glorious 1990s. Python library adapted from the original C and Matlab code by Rob Young.""",
+    license="MIT",
+    packages=find_packages(exclude=['tests*']),
+    extras_require={
+        'lxml': ['lxml'],
+        'html5lib': ['html5lib'],
+    },
+    ext_modules=[Extension('pyPyrTools.wrapConv', ['pyPyrTools/convolve.c', 'pyPyrTools/edges.c', 'pyPyrTools/wrap.c',
+                                                   'pyPyrTools/internal_pointOp.c'])],
+
+)
