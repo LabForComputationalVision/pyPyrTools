5d9745a3dddb610bc67efc4e5433611fccd3e68f
nlw0 <nwerneck@gmail.com>
2017-10-10 00:32:47 +0200

    minor

--- setup.py
+++ setup.py
@@ -9,15 +9,9 @@ setup(
     description="Steerable pyramid library for image analysis",
     long_description="""pyPyrTools implements the steerable pyramid filter bank created by Simoncelli et alii in the 
     glorious 1990s. Python library adapted from the original C and Matlab code by Rob Young.""",
-    license="MIT",
     packages=find_packages(exclude=['tests*']),
     install_requires=['numpy', 'matplotlib'],
-    extras_require={
-        'lxml': ['lxml'],
-        'html5lib': ['html5lib'],
-    },
     ext_modules=[Extension('pyPyrTools.wrapConv',
                            ['pyPyrTools/convolve.c', 'pyPyrTools/edges.c', 'pyPyrTools/wrap.c',
                             'pyPyrTools/internal_pointOp.c'])],
-
 )
--- snippets/test_pyr.py
+++ snippets/test_pyr.py
@@ -1,15 +1,14 @@
+import scipy.misc
 from pylab import *
 
 from pyPyrTools import Spyr
-
-import scipy.misc
 
 # import image to a numpy array and display
 # im = array(scipy.misc.imresize(imread('buska.png'), 0.5), dtype=float64)
 # im = array(imread('buska.png'), dtype=float64)
 
 # im = array(imread('pizza.png')[:,:,0], dtype=float64)
-im = array(scipy.misc.imresize(imread('pizza.png')[:,:,0], 0.25), dtype=float64)
+im = array(scipy.misc.imresize(imread('pizza.png')[:, :, 0], 0.25), dtype=float64)
 
 ion()
 imshow(im)
